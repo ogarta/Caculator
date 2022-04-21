a00d24754a87b8771577e12213485bb5e5cb167f
ogata-higan <lviet456@gmail.com>
2022-04-21 19:55:47 +0700

    create layout app and hide keyboard click outside

---
+++ app/src/main/java/com/example/caculator/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.caculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtNum1, edtNum2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtNum1 = findViewById(R.id.edtNum1);
+        edtNum2 = findViewById(R.id.edtNum2);
+        hideKeyboard_clickOutside();
+    }
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    public void hideKeyboard_clickOutside(){
+        edtNum1.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    hideKeyboard(v);
+                }
+            }
+        });
+
+        edtNum2.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    hideKeyboard(v);
+                }
+            }
+        });
+    }
+}
